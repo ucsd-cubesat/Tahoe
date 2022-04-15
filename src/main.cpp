46b43e7c1b97b1ead59333759c1d5aa601b0a108
Sean Boerhout <boeseany@gmail.com>
2022-04-15 15:10:31 -0700

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -73,32 +73,13 @@ void setup() {
   Serial.println(accelDevice.rebootMag());
   accelDevice.configureMag(OPER_M_CONT, ODR_M_40, POWER_M_MED, FS_M_8);
 
-  configureXLInterrupts(&accelDevice);
-  pinMode(20, INPUT);
-  pinMode(21, INPUT);
-
-  if (!accelDevice.XLGisAvailable()) {
-    Serial.println("setup failed");
-    while(1)
-      ;
-  }
-
-  Serial.println("Successfully completed setup");
+  // See demo function
+  // configureXLInterrupts(&accelDevice);
 
   // setupDS18B20(insideThermometer, sensors);
 }
 
 void loop() {
-
-  if (digitalRead(20)) {
-    Serial.println("Gryo or accel reading!");
-    delay(100);
-  }
-
-  if (digitalRead(21)) {
-    Serial.println("Mag reading!");
-    delay(100);
-  }
 
   // while (1) {
   //   gps.update(Serial1);
@@ -113,32 +94,32 @@ void loop() {
   // }
 
   // Requests unnormalized and normalized values of acceleration
-  // accelDevice.readAccelerometer(x, y, z);
-  // x_float = LSM9DS1::convertRaw(x, FS_XL_4);
-  // y_float = LSM9DS1::convertRaw(y, FS_XL_4);
-  // z_float = LSM9DS1::convertRaw(z, FS_XL_4);
-  //accelDevice.readAccelerometer(x_float, y_float, z_float);
-  //Outputs normalized values to Serial monitor
-  // Serial.print("Acceleration(m/s^2): " + String(x_float) + " " + String(y_float) + " " + String(z_float));
-  // Serial.println();
+  accelDevice.readAccelerometer(x, y, z);
+  x_float = LSM9DS1::convertRaw(x, FS_XL_4);
+  y_float = LSM9DS1::convertRaw(y, FS_XL_4);
+  z_float = LSM9DS1::convertRaw(z, FS_XL_4);
+  accelDevice.readAccelerometer(x_float, y_float, z_float);
+  // Outputs normalized values to Serial monitor
+  Serial.print("Acceleration(m/s^2): " + String(x_float) + " " + String(y_float) + " " + String(z_float));
+  Serial.println();
 
-  // //Requests values of angular velocity and outputs to Serial monitor
-  // accelDevice.readGyroscope(x_g, y_g, z_g);
-  // x_g_f = LSM9DS1::convertRaw(x_g, FS_G_2000);
-  // y_g_f = LSM9DS1::convertRaw(y_g, FS_G_2000);
-  // z_g_f = LSM9DS1::convertRaw(z_g, FS_G_2000);
-  // //accelDevice.readGyroscope(x_g_f, y_g_f, z_g_f);
-  // Serial.print("Angular Velocity(deg/s): " + String(x_g_f) + " " + String(y_g_f) + " " + String(z_g_f));
-  // Serial.println();
+  // Requests values of angular velocity and outputs to Serial monitor
+  accelDevice.readGyroscope(x_g, y_g, z_g);
+  x_g_f = LSM9DS1::convertRaw(x_g, FS_G_2000);
+  y_g_f = LSM9DS1::convertRaw(y_g, FS_G_2000);
+  z_g_f = LSM9DS1::convertRaw(z_g, FS_G_2000);
+  accelDevice.readGyroscope(x_g_f, y_g_f, z_g_f);
+  Serial.print("Angular Velocity(deg/s): " + String(x_g_f) + " " + String(y_g_f) + " " + String(z_g_f));
+  Serial.println();
   
-  // //Requests values of induction and outputs to Serial monitor
-  // accelDevice.readMagnetosensor(x_m, y_m, z_m);
-  // x_m_f = LSM9DS1::convertRaw(x_m, FS_M_8);
-  // y_m_f = LSM9DS1::convertRaw(y_m, FS_M_8);
-  // z_m_f = LSM9DS1::convertRaw(z_m, FS_M_8);
-  // //accelDevice.readMagnetosensor(x_m_f, y_m_f, z_m_f);
-  // Serial.print("Induction(gausses): " + String(x_m_f) + " " + String(y_m_f) + " " + String(z_m_f));
-  // Serial.println();
+  //Requests values of induction and outputs to Serial monitor
+  accelDevice.readMagnetosensor(x_m, y_m, z_m);
+  x_m_f = LSM9DS1::convertRaw(x_m, FS_M_8);
+  y_m_f = LSM9DS1::convertRaw(y_m, FS_M_8);
+  z_m_f = LSM9DS1::convertRaw(z_m, FS_M_8);
+  //accelDevice.readMagnetosensor(x_m_f, y_m_f, z_m_f);
+  Serial.print("Induction(gausses): " + String(x_m_f) + " " + String(y_m_f) + " " + String(z_m_f));
+  Serial.println();
 
   // //Requests temperature in Celcius
   // tempC = sensors.getTempC(insideThermometer);
